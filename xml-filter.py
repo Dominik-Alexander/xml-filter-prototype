1bbc84dd5a9374f9de28ad7928eb5b225b21f4bc
Dominik Szczepek <DSzczepek@outlook.de>
2025-03-13 00:36:22 +0100

    Sensible exception handling

--- xml-filter.py
+++ xml-filter.py
@@ -11,12 +11,18 @@ def main() -> None:
         infile = ''.join(sys.argv[1])
         tag_to_search = ''.join(sys.argv[2])
         target_path = ''.join(sys.argv[3])
+    except Exception:
+        print("Invalid input.")
+        return
 
-        # Input
+    try:
         tree = ET.parse(infile)
         root = tree.getroot()
+    except ET.ParseError:
+        print("Could not parse XML file.")
+        return
 
-        # Output
+    try:
         output_root = ET.Element(root.tag)
         outfile_list = target_path.split('/')
         path_to_outfile_list = outfile_list.copy()
@@ -24,7 +30,10 @@ def main() -> None:
         path_to_outfile = '/'.join(path_to_outfile_list)
         Path(path_to_outfile).mkdir(exist_ok=True)
         outfile = outfile_list[-1]
+    except Exception:
+        print("Could not construct output path.")
 
+    try:
         for item in root.findall(tag_to_search):
             children = list(item)
 
@@ -33,7 +42,10 @@ def main() -> None:
                 for child in children:
                     new_item.append(child)
                 output_root.append(new_item)
+    except Exception:
+        print("Could not filter XML file.")
 
+    try:
         rough_string = ET.tostring(output_root, encoding='utf-8')
         parsed_xml = xml.dom.minidom.parseString(rough_string)
         formatted_xml = parsed_xml.toprettyxml(indent="  ")
@@ -42,8 +54,7 @@ def main() -> None:
         with open(path_to_outfile + '/' + outfile, 'w', encoding='utf-8') as f:
             f.write(cleaned_xml)
     except Exception:
-        print("Well THAT didn't work now, did it?")
-        return
+        print("Could not write to output file.")
 
 if __name__ == '__main__':
     main()
