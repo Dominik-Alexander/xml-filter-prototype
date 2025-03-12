77cf50b71f8a4ea04a0f9ae6da096f3a86eb81b8
Dominik Szczepek <DSzczepek@outlook.de>
2025-03-12 23:22:43 +0100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xml-filter-prototype
+
 Prototype for a more sophisticated XML filter.
+
+## How to use
+
+It's pretty straightforward: <br>
+
+* Make sure, you have Python 3.0 or higher installed in any way shape or form.
+* Open up a terminal and run the following command: <br>
+```python xml-filter.py <input_filepath.xml> <tag_to_search> <output_filepath.xml>```
+* That's it!
+
+<br>
+Remember, however, that the paths are relative to the current directory.
---
+++ xml-filter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
+
+def main() -> None:
+    # TODO: Refactoring and better error/exception handling
+    try:
+        infile = ''.join(sys.argv[1])
+        tag_to_search = ''.join(sys.argv[2])
+        outfile = ''.join(sys.argv[3])
+
+        # Input
+        tree = ET.parse(infile)
+        root = tree.getroot()
+
+        # Output
+        output_root = ET.Element(root.tag)
+        outfile_list = outfile.split('/')
+        path_to_outfile_list = outfile_list.copy()
+        path_to_outfile_list.pop()
+        path_to_outfile = '/'.join(path_to_outfile_list)
+        Path(path_to_outfile).mkdir(exist_ok=True)
+        outfile = outfile_list[-1]
+
+        for item in root.findall(tag_to_search):
+            children = list(item)
+
+            if children:
+                new_item = ET.Element(item.tag, attrib=item.attrib)
+                for child in children:
+                    new_item.append(child)
+                output_root.append(new_item)
+
+        rough_string = ET.tostring(output_root, encoding='utf-8')
+        parsed_xml = xml.dom.minidom.parseString(rough_string)
+        formatted_xml = parsed_xml.toprettyxml(indent="  ")
+        cleaned_xml = "\n".join([line for line in formatted_xml.split("\n") if line.strip()])
+
+        with open(path_to_outfile + '/' + outfile, 'w', encoding='utf-8') as f:
+            f.write(cleaned_xml)
+    except Exception:
+        print("Well THAT didn't work now, did it?")
+        return
+
+if __name__ == '__main__':
+    main()
